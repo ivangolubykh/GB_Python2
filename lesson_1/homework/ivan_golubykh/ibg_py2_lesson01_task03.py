a16a3362b95286f4de2740f8e57ed4cafcdf33f8
ivangolubykh <ivan9@allworld.xyz>
2017-03-29 10:49:16 +0300

    Проведена подготовка в 3 заданию 1-го урока: 1) Объявлены функции, которые предполагается разработать и составлена описание-справка их работы. 2) Созданы каталоги с тестовыми данными для проверки работы функций. 3) Разработаны тесты для проверки работы функций.

---
+++ lesson_1/homework/ivan_golubykh/ibg_py2_lesson01_task03.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+import unittest
+import hashlib
+__author__ = 'Иван Голубых'
+
+
+def split_file(filename, length_fragment):
+    ''' Функция разбиения файла на кусочки указанной длины.
+     Принимает имя исходного файла и размер кусков в байтах.
+     Возвращает количество получившихся файлов-фрагментов.
+    '''
+    pass
+
+
+def creade_mda_file(dir_name, file_name_md5):
+    ''' Функция составления текстового файла с md5-хэшами файлов в
+    указанной директории.
+     Принимает имя директории с файлами, имя файла для записи результатов.
+     Возвращает количество просмотренных файлов.
+    '''
+    pass
+
+
+def join_file(dir_name, file_name_md5, file_name_joined):
+    ''' Функция "склеивания" файла на основе упорядоченных хэш-сумм.
+    Используются только имена файлов без расширения.
+     Принимает имя директории с файлами-кусочками, имя файла с
+    хэш-суммами, имя выходного файла.
+     Возвращает размер полученного файла.
+    '''
+    pass
+
+
+def main():
+    ''' Главная функция должна:
+    1) распаковать все архивы в нужном каталоге
+    2) Вызвать функцию склеивания для каждого каталога и получит склеенные
+    файлы.
+    3) определить тип получившихся фалов и вывести в консоль.
+    4) вернуть значение 0 (ноль), когда будет написана отлажена эта функция.
+    '''
+    return 1
+
+
+class TestThis(unittest.TestCase):
+    def test_split_file(self):
+        ''' Для этого теста создал папку test_split_file с 1 фалом
+        '''
+        self.assertEqual(creade_mda_file('./test_split_file/README.MD', 1000),
+                         4)
+
+    def test_creade_mda_file(self):
+        ''' Для этого теста создал папку test_creade_mda_file с 3 фалами
+        '''
+        self.assertEqual(creade_mda_file('./test_creade_mda_file',
+                                         './test_creade_mda_file/parts.md5'
+                                         ),
+                         3)
+
+    def test_join_file(self):
+        ''' Для этого теста использую папку test_creade_mda_file. Использую
+        только имена файлов без расширения.
+        '''
+        self.assertEqual(creade_mda_file('./test_split_file/README.MD', 1000),
+                         2313)
+
+    def test_main(self):
+        self.assertEqual(main(), 0)
+
+
+if __name__ == '__main__':
+    main()
